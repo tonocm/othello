75ac7ce65e3a5be326ec9d008cfd01c23c724a3b
Colin Pronovost <colin.pron@live.com>
2015-05-01 20:17:57 -0400

    Fix old version of alphaBeta creeping back in

--- playAntonio.cpp
+++ playAntonio.cpp
@@ -192,28 +192,6 @@ void makeMove(int x, int y)
 /* player 1 is max player, player -1 is min player */
 State alphaBeta(State state, int depth, int alpha, int beta, int player)
 {
-    int i;
-    int value;
-    if(cutoffTest(state, depth))
-    {
-	makeMove(best.move[0], best.move[1]);
-	return best;
-    }
-    best = NULL; /* Handles early pruning or no possible moves */
-    if(player == 1){ //max player
-	int i;
-	int value;
-	if(cutoffTest(state, depth))
-	{
-		makeMove(best.move[0], best.move[1]);
-		return best;
-	}
-	best = NULL; /* Handles early pruning or no possible moves */
-	if(player == 1){ //max player
-    
-    for(State action : actions(state, player)){ // This line requires C++11
-      (child, unused) = result(state, action); //todo
-      value = alphaBeta(child, depth+1, alpha, beta, -player);
   static State best = NULL;
   int i;
   int value;
