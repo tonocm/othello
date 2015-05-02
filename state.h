6247d06af63d19db582e6e34db56885aa062a747
Colin Pronovost <colin.pron@live.com>
2015-05-01 21:56:17 -0400

    Fix some type errors

--- state.h
+++ state.h
@@ -27,18 +27,16 @@ public:
     memcpy(move, s.move, sizeof(int) * 2);
   }
 
-  State(int board[SIZE][SIZE]){
-    for(int i = 0; i<SIZE; i++)
-      for(int j = 0; j<SIZE; j++)
-	_state[i][j] = board[i][j];
+  State(Value board[SIZE][SIZE]){
+    memcpy(_state, board, sizeof(Value) * SIZE * SIZE);
     parent = NULL;
     move[0] = move[1] = -1;
   }
-
+111
   Value *operator[] (size_t idx) {
     return _state[idx];
   }
 private:
   Value _state[SIZE][SIZE];
-  State *parent;
+  const State *parent;
 };
